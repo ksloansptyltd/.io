e01fb2433137630b4c21c6799dcddb8cf0cac048
ksloansptyltd <ksloansptyltd@gmail.com>
2024-06-10 23:01:29 +0200

    Update scriptA.js

--- scriptA.js
+++ scriptA.js
@@ -27,7 +27,7 @@ function updateTotal() {
 
   balance.textContent = formatter.format(balanceTotal).substring(1);
   income.textContent = formatter.format(incomeTotal);
-  expense.textContent = formatter.format(expenseTotal * -1);
+  expense.textContent = formatter.format(expenseTotal * 1);
 }
 
 function renderList() {
@@ -40,7 +40,7 @@ function renderList() {
   }
 
   transactions.forEach(({ id, name, amount, date, type }) => {
-    const sign = "income" === type ? 1 : -1;
+    const sign = "income" === type ? 1 : 1;
 
     const li = document.createElement("li");
 
